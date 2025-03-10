b6a5f646aa2bc1e7db478e7c5794d92cd7d2ef67
bananpalme <nigginz12@gmail.com>
2025-03-10 02:11:17 +0100

    added icons

--- app/src/main/java/com/example/storeapp_filter/MainActivity.kt
+++ app/src/main/java/com/example/storeapp_filter/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.storeapp_filter
 
 import android.os.Bundle
+import androidx.compose.foundation.Image
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,30 +14,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.storeapp_filter.ui.theme.StoreApp_filterTheme
-import androidx.compose.material3.Icon
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.sp
 
 
 class MainActivity : ComponentActivity() {
@@ -56,74 +48,275 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    Scaffold(
-        bottomBar = { FooterBar() }
-    ) { innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding)
-                .padding(16.dp)
+                .background(Color(0xFFF1eadE))
         ) {
             Text(
                 text = "Figma",
-                fontSize = 12.sp,
-                fontWeight = FontWeight.SemiBold,
-                modifier = Modifier.padding(bottom = 8.dp)
+                modifier = Modifier.padding(16.dp),
+                fontWeight = FontWeight.SemiBold
             )
 
-            var searchQuery by remember { mutableStateOf("") }
-            TextField(
-                value = searchQuery,
-                onValueChange = { searchQuery = it },
-                placeholder = { Text("Search") },
+            Row(
                 modifier = Modifier
+                    .padding(start = 32.dp, end = 32.dp)
+                    .background(Color.White)
                     .fillMaxWidth()
-                    .height(40.dp)
-            )
+
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.search),
+                    contentDescription = "Mit PNG billede",
+                    modifier = Modifier
+                        .width(20.dp)
+                        .height(20.dp)
+                    ,
+                    contentScale = ContentScale.Crop
+                )
+                Text(
+                    text = "Search"
+                )
+            }
 
             Spacer(modifier = Modifier.height(20.dp))
 
-            Box(
+            Column(
                 modifier = Modifier
                     .fillMaxWidth()
                     .weight(1f)
-                    .background(Color.Gray, shape = RoundedCornerShape(16.dp))
-            )
+                    .padding(16.dp)
+                    .background(
+                        Color(0xFFFFFFFF),
+                        shape = RoundedCornerShape(16.dp)
+                    ),
+                    horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Filtre",
+                    modifier = Modifier
+                        .padding(25.dp),
+                    fontWeight = FontWeight.Bold
+                )
+                Row(modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth()
+                    .height(35.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically) { Text(text = "Kategori")
+                    Image(
+                        painter = painterResource(id = R.drawable.arrow),
+                        contentDescription = "Mit PNG billede",
+                        modifier = Modifier
+                            .width(35.dp)
+                            .height(35.dp)
+                        ,
+                        contentScale = ContentScale.Crop
+                    )
+                }
+
+                HorizontalDivider(thickness = 1.dp, color = Color.LightGray)
+
+                Row(modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth()
+                    .height(35.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically) { Text(text = "Størrelse")
+                    Image(
+                        painter = painterResource(id = R.drawable.arrow),
+                        contentDescription = "Mit PNG billede",
+                        modifier = Modifier
+                            .width(35.dp)
+                            .height(35.dp)
+                        ,
+                        contentScale = ContentScale.Crop
+                    )
+                }
+
+                HorizontalDivider(thickness = 1.dp, color = Color.LightGray)
+
+                Row(modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth()
+                    .height(35.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically) { Text(text = "Mærke")
+                    Image(
+                        painter = painterResource(id = R.drawable.arrow),
+                        contentDescription = "Mit PNG billede",
+                        modifier = Modifier
+                            .width(35.dp)
+                            .height(35.dp)
+                        ,
+                        contentScale = ContentScale.Crop
+                    )
+                }
+
+                HorizontalDivider(thickness = 1.dp, color = Color.LightGray)
+
+                Row(modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth()
+                    .height(35.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically) { Text(text = "Farve")
+                    Image(
+                        painter = painterResource(id = R.drawable.arrow),
+                        contentDescription = "Mit PNG billede",
+                        modifier = Modifier
+                            .width(35.dp)
+                            .height(35.dp)
+                        ,
+                        contentScale = ContentScale.Crop
+                    )
+                }
+
+                HorizontalDivider(thickness = 1.dp, color = Color.LightGray)
+
+                Row(modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth()
+                    .height(35.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically) { Text(text = "Tilstand")
+                    Image(
+                        painter = painterResource(id = R.drawable.arrow),
+                        contentDescription = "Mit PNG billede",
+                        modifier = Modifier
+                            .width(35.dp)
+                            .height(35.dp)
+                        ,
+                        contentScale = ContentScale.Crop
+                    )
+                }
+
+                HorizontalDivider(thickness = 1.dp, color = Color.LightGray)
+
+                Row(modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth()
+                    .height(35.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically) { Text(text = "Materiale")
+                    Image(
+                        painter = painterResource(id = R.drawable.arrow),
+                        contentDescription = "Mit PNG billede",
+                        modifier = Modifier
+                            .width(35.dp)
+                            .height(35.dp)
+                        ,
+                        contentScale = ContentScale.Crop
+                    )
+                }
+
+                HorizontalDivider(thickness = 1.dp, color = Color.LightGray)
+
+                Row(modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth()
+                    .height(35.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically) { Text(text = "Placering")
+                    Image(
+                        painter = painterResource(id = R.drawable.arrow),
+                        contentDescription = "Mit PNG billede",
+                        modifier = Modifier
+                            .width(35.dp)
+                            .height(35.dp)
+                        ,
+                        contentScale = ContentScale.Crop
+                    )
+                }
+
+            }
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.White)
+                    ,
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Column {
+                        Image(
+                            painter = painterResource(id = R.drawable.home),
+                            contentDescription = "Mit PNG billede",
+                            modifier = Modifier
+                                .width(35.dp)
+                                .height(35.dp)
+                            ,
+                            contentScale = ContentScale.Crop
+                        )
+                        Text(
+                            text = "Home",
+                            fontSize = 14.sp
+                        )
+                    }
+                    Column {
+                        Image(
+                            painter = painterResource(id = R.drawable.search),
+                            contentDescription = "Mit PNG billede",
+                            modifier = Modifier
+                                .width(35.dp)
+                                .height(35.dp)
+                            ,
+                            contentScale = ContentScale.Crop
+                        )
+                        Text(
+                            text = "Search",
+                            fontSize = 14.sp
+                        )
+                    }
+                    Image(
+                        painter = painterResource(id = R.drawable.plus),
+                        contentDescription = "Mit PNG billede",
+                        modifier = Modifier
+                            .width(50.dp)
+                            .height(50.dp)
+                        ,
+                        contentScale = ContentScale.Crop
+                    )
+                    Column {
+                        Image(
+                            painter = painterResource(id = R.drawable.inbox),
+                            contentDescription = "Mit PNG billede",
+                            modifier = Modifier
+                                .width(35.dp)
+                                .height(35.dp)
+                            ,
+                            contentScale = ContentScale.Crop
+                        )
+                        Text(
+                            text = "Inbox",
+                            fontSize = 14.sp
+                        )
+                    }
+                    Column {
+                        Image(
+                            painter = painterResource(id = R.drawable.account),
+                            contentDescription = "Mit PNG billede",
+                            modifier = Modifier
+                                .width(35.dp)
+                                .height(35.dp)
+                            ,
+                            contentScale = ContentScale.Crop
+                        )
+                        Text(
+                            text = "Account",
+                            fontSize = 14.sp
+                        )
+                    }
+                }
+            }
+
         }
-    }
-}
 
-
-@Composable
-fun FooterBar() {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(60.dp)
-            .background(Color.LightGray),
-        contentAlignment = Alignment.Center
-    ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            FooterItem(Icons.Default.Home, "Home")
-            FooterItem(Icons.Default.Search, "Search")
-            FooterItem(Icons.Default.Person, "Profile")
-        }
-    }
-}
-
-@Composable
-fun FooterItem(icon: ImageVector, label: String) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Icon(imageVector = icon, contentDescription = label, modifier = Modifier.size(24.dp))
-        Text(text = label, fontSize = 12.sp)
-    }
-}
 
 
 @Preview(showBackground = true)
